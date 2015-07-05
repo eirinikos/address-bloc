b530abe96a6682756e8bf78f98cfc003bf393351
Andrea <eirinikos@gmail.com>
2015-07-05 11:01:38 -0700

    checkpoint 21 completed - ruby models

---
+++ models/address_book.rb
@@ -0,0 +1,33 @@
+
+
+# Using require_relative, tell Ruby to load
+# the library named entry.rb relative to address_book.rb's filepath
+require_relative "entry.rb"
+
+class AddressBook
+  attr_accessor :entries
+  
+  def initialize
+    @entries = []
+  end
+
+  def add_entry(name,phone,email)
+
+    # Create index to store the insertion index.
+    index = 0
+    @entries.each do |entry|
+
+      # Compare name with the name of the current entry.
+      # If name lexicographically proceeds entry.name, we've 
+      # found the index to insert at. Otherwise, we increment
+      # index and continue comparing with other entries.
+      if name < entry.name
+        break
+      end
+      index += 1
+    end
+
+    # Insert a new entry into entries using the calculated index.
+    @entries.insert(index, Entry.new(name,phone,email))
+  end
+end
