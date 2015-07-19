d7a99ffb3e0104c2487102eea2886be97e2bb09a
Andrea <eirinikos@gmail.com>
2015-07-19 16:19:02 -0700

    checkpoint 22 completed, menus

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,3 +1,15 @@
 
 
+require_relative "controllers/menu_controller"
+
+
+menu = MenuController.new
+# create a new menu controller when AddressBloc starts.
+
+system "clear"
+# clear the command line.
+
 puts "Welcome to AddressBloc!"
+
+menu.main_menu
+# call main_menu to display the menu
